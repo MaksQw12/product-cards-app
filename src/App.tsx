c0dcd70ecab490d0b6175f3dcc782ffd6ea0a6c9
admin <Maksim7142@yandex.ru>
2024-12-04 22:48:27 +0300

    basic structure project and installing libraries

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app-wrapper">
+      <header>Top</header>
+      <main className="app-content">Main Content</main>
+      <footer>Bot</footer>
+    </div>
+  );
+}
+
+export default App;
